9ac584404c5f9b0a5d1487f99089f49cc2b7c45d
Paul Collins <paul.collins@brightscope.com>
2013-05-31 11:01:45 -0700

    Add support for the zsh-git-prompt
    zsh-git-prompt written by olivierverdier.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,18 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Installs the ZSH-Git-Prompt
+#
+# You must modify your own .zshrc file to include it and activate it on the
+# prompt
+
+class zshgitprompt {
+  require zsh
+
+  file{"${boxen::config::envdir}/zshgitprompt.sh":
+    content => template('zshgitprompt/zshgitprompt.sh.erb')
+  }
+
+  repository{
+    "${boxen::config::datadir}/zshgitprompt":
+      source   => 'olivierverdier/zsh-git-prompt',
+      provider => 'git'
+  }
 }
---
+++ spec/classes/zshgitprompt_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+# Rename this file to classname_spec.rb
+# Check other boxen modules for examples
+# or read http://rspec-puppet.com/tutorial/
+describe 'zshgitprompt' do
+  let(:facts) do
+    {
+      :luser      => 'pcollins',
+      :boxen_home => '/opt/boxen'
+    }
+  end
+
+  it do
+    should contain_repository("/opt/boxen/data/zshgitprompt").with({
+      :source   => 'olivierverdier/zsh-git-prompt',
+      :provider => 'git'
+    })
+  end
+end
